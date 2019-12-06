872b356f621850a6d53f5cb4aa72ff652d44af63
tobiaswirtz <tobias.wirtz@tum.de>
2019-12-06 19:21:14 +0100

    Working version

--- camelbob.py
+++ camelbob.py
@@ -1,13 +1,17 @@
 import os
 import sys
+import textwrap
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 
-def getSize(strLen):
-	strSpan = 512 - 1
-	fontSpan = 54 - 6
-	valueScaled = float(-strLen - 1) / float(strSpan)
-	return 6 + int(valueScaled * fontSpan)
+def formatString(s):
+	newStr = ""
+	for i in range(len(s)):
+		if i % 2 == 0:
+			newStr = newStr + s[i].lower()
+		else:
+			newStr = newStr + s[i].upper()
+	return newStr
 
 inputStr = sys.argv[1]
 
@@ -17,6 +21,28 @@ filepath = "imgs/" + d + ".jpg"
 os.system("cp Mocking-Spongebob.jpg " + filepath)
 
 image = Image.open(filepath)
+W, H = image.size
+
+inputLength = len(inputStr)
 
 canvas = ImageDraw.Draw(image)
-font  = ImageFont.truetype('Impact.ttf', size=getSize(len(inputStr)))
+
+inputStr = formatString(inputStr)
+font  = ImageFont.truetype('Impact.ttf', size=23)
+shadowcolor = "black"
+lines = textwrap.wrap(inputStr, width=40)
+y_text = 275 - (len(lines) * 6)
+for line in lines:
+	width, height = canvas.textsize(line, font=font)
+	canvas.text((((W-width)/2) - 1, y_text), line, font=font, fill=shadowcolor)
+	canvas.text((((W-width)/2) + 1, y_text), line, font=font, fill=shadowcolor)
+	canvas.text((((W-width)/2), y_text - 1), line, font=font, fill=shadowcolor)
+	canvas.text((((W-width)/2), y_text + 1), line, font=font, fill=shadowcolor)
+	canvas.text((((W-width)/2), y_text), line, font=font)
+	y_text += height
+
+canvas = ImageDraw.Draw(image)
+
+image.save(filepath)
+command = "impbcopy " + filepath
+os.system(command)
